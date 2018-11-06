19df1acd5c8e96353bb024a794ede7ad1f11b87e
jasonhoendervanger <hoendervanger@eur.nl>
2018-11-06 20:49:29 +0100

    Added real-time messaging system.

--- Domain/Entities/PandaGroup/PandaGroupRole.php
+++ Domain/Entities/PandaGroup/PandaGroupRole.php
@@ -4,7 +4,15 @@ namespace Domain\Entities\PandaGroup;
 
 use Domain\Common\Entity;
 
+/**
+ * Class PandaGroupRole
+ *
+ * @package Domain\Entities\PandaGroup
+ */
 class PandaGroupRole extends Entity
 {
-
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'label'];
 }
